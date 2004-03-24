a18f82284100794413a2005bee32df5b6569021d
Jeremy Newman <jnewman@winehq.org>
2004-03-24 15:52:09 +0000

    added

---
+++ tables/session_list.sql
@@ -0,0 +1,13 @@
+use apidb;
+
+DROP TABLE IF EXISTS session_list;
+
+CREATE TABLE session_list (
+ session_id varchar(64) NOT NULL default '',
+ userid int(11) default NULL,
+ ip varchar(64) default NULL,
+ data text,
+ messages text,
+ stamp timestamp(14) NOT NULL,
+ PRIMARY KEY (session_id)
+) TYPE=MyISAM;
