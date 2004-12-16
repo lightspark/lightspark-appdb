5dea9ec1109d3bde1a37867cdc07dbb31bf27fa6
Paul van Schayck <polleke@gmail.com>
2004-12-16 02:24:55 +0000

    Valid HTML 4.01 popups

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
-
 function openWin(fileToOpen,nameOfWindow,width,height) {
 	myWindow = window.open("",nameOfWindow,"menubar=no,scrollbars=yes,status=no,width="+width+",height="+height);
 	myWindow.document.open();
-	myWindow.document.write('<HTML><HEAD><TITLE>ScreenShot Viewer</TITLE>')
-	myWindow.document.write('<BODY BGCOLOR="#000000" TEXT="#FFFFFF" topmargin="0" leftmargin="0" marginwidth="0" marginheight="0">');
-	myWindow.document.write('<a href="javascript:self.close();"><img src="'+ fileToOpen +'" border=0></a>');
-	myWindow.document.write('</BODY></HTML>');
+	myWindow.document.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">');
+	myWindow.document.write('<html><head><title>Screenshot Viewer</title>')
+	myWindow.document.write('<style type="text/css">');
+	myWindow.document.write('body { margin: 0; padding: 0; background-color: black; }');
+	myWindow.document.write('img { border: 0; }');
+	myWindow.document.write('p { display: inline; }');
+	myWindow.document.write('</style></head><body><p>');
+	myWindow.document.write('<a onclick="self.close();" href=""><img src="'+ fileToOpen +'" alt="Screenshot"></a>');
+	myWindow.document.write('</p></body></html>');
 	myWindow.document.close();
 }
 
@@ -14,5 +18,3 @@ function deleteURL(text, url) {
 	   self.location = url;
 	}
 }
-
-
