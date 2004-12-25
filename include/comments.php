48cfee1c51193cb49612b0edd0b5b105b6895c35
Tony Lambregts <tony_lambregts@telusplanet.net>
2004-12-25 19:48:06 +0000

    Fix up displaying comments when not logged in.

--- include/comments.php
+++ include/comments.php
@@ -63,7 +63,7 @@ function view_app_comment($ob)
     echo "</td></tr>\n";
 
     // delete message button, for admins
-    if(havepriv("admin") || $_SESSION['current']->is_maintainer($ob->appId, $ob->versionId))
+    if (loggedin() && (havepriv("admin") || $_SESSION['current']->is_maintainer($appId,$versionId) ))
     {
         echo "<tr>";
         echo "<td><form method=\"post\" name=\"message\" action=\"".BASE."deletecomment.php\"><input type=submit value='Delete' class=button>\n";
