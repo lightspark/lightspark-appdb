39870dc00852236a49f2c7feb0988dc0ae52fdbe
Paul van Schayck <polleke@gmail.com>
2004-12-29 18:42:34 +0000

    Add functions to compile a update or insert query

--- include/db.php
+++ include/db.php
@@ -35,4 +35,50 @@ function query_userdb($sQuery)
     }
     return $hResult;
 }
+
+/**
+* Expects an array in this form:
+* $aFoo['field'] = 'value';
+* 
+* Returns an array ready to be put in a query like this
+* $sQuery = "INSERT INTO `foo` {$aReturn['FIELDS']} VALUES {$aReturn['VALUES']}";
+* 
+* Values are addslashes()'d.
+*/
+
+function compile_insert_string($aData)
+{
+    foreach ($aData as $k => $v)
+    {
+        $field_names .= "`$k`,";
+        $field_values .= "'".addslashes($v)."',";
+    }
+
+    // Get rid of the end ,
+    $field_names  = preg_replace( "/,$/" , "" , $field_names  );
+    $field_values = preg_replace( "/,$/" , "" , $field_values );
+
+    return array('FIELDS' => $field_names, 'VALUES' => $field_values);
+}
+
+/**
+* Expects an array in this form:
+* $aFoo['field'] = 'value';
+* 
+* Returns a string ready to be put in a query like this
+* $sQuery = "UPDATE `foo` $sReturn";
+* 
+* Values are addslashes()'d.
+*/
+function compile_update_string($aData)
+{
+    foreach ($aData as $k => $v) 
+    {
+        $return .= "`$k`='".addslashes($v)."',";
+    }
+    
+    $return = preg_replace( "/,$/" , "" , $return );
+    
+    return $return;
+}
 ?>
