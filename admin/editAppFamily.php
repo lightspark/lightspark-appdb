72150edc8038db0aabca18dbb0dd018194e326f6
Paul van Schayck <polleke@gmail.com>
2005-01-02 16:51:48 +0000

    - Convert to superglobals - Use include/db.php - Variable naming - Misc fixes

--- admin/editAppFamily.php
+++ admin/editAppFamily.php
@@ -1,10 +1,18 @@
 <?php
-
+/**********************************/
+/* Edit application family        */
+/**********************************/
 
 include("path.php");
 include(BASE."include/"."incl.php");
 include(BASE."include/"."tableve.php");
 include(BASE."include/"."qclass.php");
+
+if(!is_numeric($_REQUEST['appId']))
+{
+    errorpage("Wrong ID");
+    exit;
+}
 
 if(!(havepriv("admin") || $_SESSION['current']->is_super_maintainer($_REQUEST['appId'])))
 {
@@ -12,148 +20,110 @@ if(!(havepriv("admin") || $_SESSION['current']->is_super_maintainer($_REQUEST['a
     exit;
 }
 
-if($_POST)
+if(isset($_REQUEST['submit']))
 {
     $statusMessage = '';
+    
     // commit changes of form to database
-    if($submit1 == "Update Database")
+    if($_REQUEST['submit'] == "Update Database")
     {
-        $statusMessage = '';
-        $appName       = addslashes($appName);
-        $description   = addslashes($description);
-        $webPage       = addslashes($webPage);
-        if (!mysql_query("UPDATE appFamily SET appName = '".$appName."', ".
-            "vendorId = $vendorId, keywords = '".$keywords."', ".
-            "description = '".$description."', ".
-            "webPage = '".$webPage."', ".
-            "catId = $catId".
-            " WHERE appId = $appId"))
-        {
-            $statusMessage = "<p><b>Database Error!<br>".mysql_error()."</b></p>\n";
-            addmsg($statusMessage, "red");
-	}
-        else
+        $sUpdate = compile_update_string(array( 'appName' => $_REQUEST['appName'],
+                                               'description' => $_REQUEST['description'],
+                                               'webPage' => $_REQUEST['webPage'],
+                                               'vendorId' => $_REQUEST['vendorId'],
+                                               'keywords' => $_REQUEST['keywords'],
+                                               'catId' =>  $_REQUEST['catId'] ));
+                                               
+        if (query_appdb("UPDATE `appFamily` SET $sUpdate WHERE `appId` = {$_REQUEST['appId']}"))
             addmsg("Database Updated", "green");
     }
-    else if($submit1 == "Update URL")
+    else if($_REQUEST['submit'] == "Update URL")
     {
-        //process added URL
-        if(debugging()) { echo "<p align=center><b>$url:</b> $url_desc </p>"; }
+        if (!empty($_REQUEST['url_desc']) && !empty($_REQUEST['url']) )
+        {
+            // process added URL
+            if(debugging()) { echo "<p align=center><b>{$_REQUEST['url']}:</b> {$_REQUEST['url_desc']} </p>"; }
         
-        if ($url_desc && $url )
-        {
-            $query = "INSERT INTO appData VALUES (null, $appId, 0, 'url','$url_desc', '$url')";
+            $aInsert = compile_insert_string( array( 'appId' => $_REQUEST['appId'],
+                                             'type' => 'url',
+                                             'description' => $_REQUEST['url_desc'],
+                                             'url' => $_REQUEST['url']));
+            
+            $sQuery = "INSERT INTO appData ({$aInsert['FIELDS']}) VALUES ({$aInsert['VALUES']})";
 	    
-            if(debugging()) { echo "<p align=center><b>query:</b> $query </p>"; }
+            if(debugging()) { echo "<p align=center><b>query:</b> $sQuery </p>"; }
 	    
-            if (mysql_query($query))
-            {
-            //success
+            if (query_appdb($sQuery))
                 addmsg("The URL was successfully added into the database", "green");
-            }
-            else
-            {
-                //error
-                $statusMessage = "<p><b>Database Error!<br>".mysql_error()."</b></p>\n";
-                addmsg($statusMessage, "red");
-            }
-        }
-        else if ($url_desc != $url) // not both blank
-        {
-            addmsg("The URL or description was blank. URL not added into the database", "red");
         }
         
         // Process changed URL's
-        for($i = 0; $i < $rows; $i++)
+        
+        for($i = 0; $i < $_REQUEST['rows']; $i++)
         {
-            if(debugging()) { echo "<p align=center><b>$adescription[$i]:</b> $aURL[$i]: $adelete[$i] : $aId[$i] : .$aOldDesc[$i]. : $aOldURL[$i]</p>"; }
+            if(debugging()) { echo "<p align=center><b>{$_REQUEST['adescription'][$i]}:</b> {$_REQUEST['aURL'][$i]}: {$_REQUEST['adelete'][$i]} : {$_REQUEST['aId'][$i]} : .{$_REQUEST['aOldDesc'][$i]}. : {$_REQUEST['aOldURL'][$i]}</p>"; }
             
-            if ($adelete[$i] == "on")
+            if ($_REQUEST['adelete'][$i] == "on")
             {
-                if(debugging()) { echo "<p align=center><b>$adescription[$i]:</b> $aURL[$i]: $adelete[$i] : $aId[$i] : $aOldDesc[$i] : $aOldURL[$i]</p>"; }
-	        $result = mysql_query("DELETE FROM appData WHERE id = '$aId[$i]'");
+	            $hResult = query_appdb("DELETE FROM appData WHERE id = '{$_REQUEST['aId'][$i]}'");
 
-                if(!$result)
-                {
-                    //error
-                    $statusMessage = "<p><b>Database Error!<br>".mysql_error()." deleting URL ".$aOldDesc[$i]." (".$aOldURL[$i].")</b></p>\n";
-                    addmsg($statusMessage, "red");
-                    $i = $rows+1;
-                }
-                else
-                {
-                    $statusMessage = "<p><b>Successfully deleted URL ".$aOldDesc[$i]." (".$aOldURL[$i].")</b></p>\n";
-                    addmsg($statusMessage, "green");
-                }  
+                if($hResult)
+                    addmsg("<p><b>Successfully deleted URL ".$_REQUEST['aOldDesc'][$i]." (".$_REQUEST['aOldURL'][$i].")</b></p>\n",'green');
+
             }
-            else if( $aURL[$i] != $aOldURL[$i] || $adescription[$i] != $aOldDesc[$i])
+            else if( $_REQUEST['aURL'][$i] != $_REQUEST['aOldURL'][$i] || $_REQUEST['adescription'][$i] != $_REQUEST['aOldDesc'][$i])
             {
-                if(!$aURL[$i] || !$adescription[$i])
+                if(empty($_REQUEST['aURL'][$i]) || empty($_REQUEST['adescription'][$i]))
                     addmsg("The URL or description was blank. URL not changed in the database", "red");
                 else
                 {
-                    if(debugging()) { echo "<p align=center><b>$adescription[$i]:</b> $aURL[$i]: $adelete[$i] : $aId[$i] : $aOldDesc[$i] : $aOldURL[$i]</p>"; }
-                    $adescription[$i] = addslashes($adescription[$i]);
-                    $aURL[$i] = addslashes($aURL[$i]);
-                    if (!mysql_query("UPDATE appData SET description = '".$adescription[$i]."' , url = '".$aURL[$i]."'".
-                        " WHERE Id = $aId[$i]"))
-                    {
-                        //error
-                        $statusMessage = "<p><b>Database Error!<br>".mysql_error()." updateing URL ".$aOldDesc[$i]." (".$aOldURL[$i].")</b></p>\n";
-                        addmsg($statusMessage, "red");
-                       $i = $rows+1;
-                    }
-                    else
-                    {
-                         $statusMessage = "<p><b>Successfully updated ".$aOldDesc[$i]." (".$aOldURL[$i].")</b></p>\n";
-                         addmsg($statusMessage, "green");
-                    }
+                    $sUpdate = compile_update_string( array( 'description' => $_REQUEST['adescription'][$i],
+                                                     'url' => $_REQUEST['aURL'][$i]));
+                    if (query_appdb("UPDATE appData SET $sUpdate WHERE id = '{$_REQUEST['aId'][$i]}'"))
+                         addmsg("<p><b>Successfully updated ".$_REQUEST['aOldDesc'][$i]." (".$_REQUEST['aOldURL'][$i].")</b></p>\n",'green');
                 }
             }            
         }
     }
+    
+    redirect(apidb_fullurl("appview.php?appId={$_REQUEST['appId']}"));
 }
-//Show the form for editing the Application Family 
+// Show the form for editing the Application Family 
 {
     $family = new TableVE("edit");
 
-    $result = mysql_query("SELECT appId, appName, vendorId, keywords, ".
-			      "description, webPage, catId from appFamily WHERE ".
-			      "appId = '$appId'");
-    if(!$result)
+    $result = query_appdb("SELECT * from appFamily WHERE appId = '{$_REQUEST['appId']}'");
+    
+    if(!mysql_num_rows($result))
     {
-        errorpage("You must be logged in to edit preferences");
+        errorpage('Application does not exist');
         exit;
     }
+    
+    $ob = mysql_fetch_object($result);
+    
+    if(debugging()) { echo "<p align=center><b>appName:</b> $ob->appName </p>"; }
 
-    list($appId, $appName, $vendorId, $keywords, $description, $webPage, $catId) = mysql_fetch_row($result);
-    if(debugging()) { echo "<p align=center><b>appName:</b> $appName </p>"; }
-
-    // show edit app family form
-    $table = "appFamily";
-    $query = "SELECT * FROM $table WHERE appId = $appId";
-
-    if(debugging()) { echo "<p align=center><b>query:</b> $query </p>"; }
-    apidb_header("Edit Application Family");
+     apidb_header("Edit Application Family");
 
     echo "<form method=post action='editAppFamily.php'>\n";
-    echo html_frame_start("Data for Application ID $appId", "90%","",0);
+    echo html_frame_start("Data for Application ID $ob->appId", "90%","",0);
     echo html_table_begin("width='100%' border=0 align=left cellpadding=6 cellspacing=0 class='box-body'");
 
-    echo '<input type=hidden name="appId" value='.$appId.'>';
-    echo '<tr><td class=color1>Name</td><td class=color0><input size=80% type="text" name="appName" type="text" value="'.$appName.'"></td></tr>',"\n";
+    echo '<input type=hidden name="appId" value='.$ob->appId.'>';
+    echo '<tr><td class=color1>Name</td><td class=color0><input size=80% type="text" name="appName" type="text" value="'.$ob->appName.'"></td></tr>',"\n";
     echo '<tr><td class=color4>Vendor</td><td class=color0>';
-    $family->make_option_list("vendorId", $vendorId, "vendor", "vendorId", "vendorName");
+    $family->make_option_list("vendorId", $ob->vendorId, "vendor", "vendorId", "vendorName");
     echo '</td></tr>',"\n";
-    echo '<tr><td class=color1>Keywords</td><td class=color0><input size=80% type="text" name="keywords" value="'.$keywords.'"></td></tr>',"\n";
+    echo '<tr><td class=color1>Keywords</td><td class=color0><input size=80% type="text" name="keywords" value="'.$ob->keywords.'"></td></tr>',"\n";
     echo '<tr><td class=color4>Description</td><td class=color0>', "\n";
-    echo '<textarea cols=50 rows=10 name="description">'.stripslashes($description).'</textarea></td></tr>',"\n";
-    echo '<tr><td class=color1>Web Page</td><td class=color0><input size=80% type="text" name="webPage" value="'.$webPage.'"></td></tr>',"\n";
+    echo '<textarea cols=50 rows=10 name="description">'.stripslashes($ob->description).'</textarea></td></tr>',"\n";
+    echo '<tr><td class=color1>Web Page</td><td class=color0><input size=80% type="text" name="webPage" value="'.$ob->webPage.'"></td></tr>',"\n";
     echo '<tr><td class=color4>Category</td><td class=color0>';
-    $family->make_option_list("catId", $catId, "appCategory", "catId", "catName");
+    $family->make_option_list("catId", $ob->catId, "appCategory", "catId", "catName");
     echo '</td></tr>',"\n";
 
-    echo '<tr><td colspan=2 align=center class=color3><input type="submit" name=submit1 value="Update Database"></td></tr>',"\n";
+    echo '<tr><td colspan=2 align=center class=color3><input type="submit" name=submit value="Update Database"></td></tr>',"\n";
 
     echo html_table_end();
     echo html_frame_end();
@@ -165,7 +135,7 @@ if($_POST)
     echo '<table border=0 cellpadding=6 cellspacing=0 width="100%">',"\n";
             
     $i = 0;
-    $result = mysql_query("SELECT * FROM appData WHERE appId = $appId AND type = 'url' AND versionId = 0");
+    $result = mysql_query("SELECT * FROM appData WHERE appId = $ob->appId AND type = 'url' AND versionId = 0");
     if($result && mysql_num_rows($result) > 0)
     {
         echo '<tr><td class=color1><b>Delete</b></td><td class=color1>',"\n";
@@ -179,30 +149,30 @@ if($_POST)
             $temp4 = "aOldDesc[".$i."]";
             $temp5 = "aOldURL[".$i."]";
             echo '<tr><td class=color3><input type="checkbox" name="'.$temp0.'"></td>',"\n";
-            echo '<td class=color3><input size=45% type="text" name = "'.$temp1.'" value ="'.stripslashes($ob->description).'"</td>',"\n";
+            echo '<td class=color3><input size=45% type="text" name="'.$temp1.'" value ="'.stripslashes($ob->description).'"</td>',"\n";
             echo '<td class=color3><input size=45% type="text" name="'.$temp2.'" value="'.$ob->url.'"></td></tr>',"\n";
             echo '<input type=hidden name="'.$temp3.'" value='.$ob->id.'>';
             echo '<input type=hidden name="'.$temp4.'" value="'.stripslashes($ob->description).'">';
             echo '<input type=hidden name="'.$temp5.'" value="'.$ob->url.'">',"\n";
             $i++;
-	}
+        }
     } else
     {
         echo '<tr><td class=color1></td><td class=color1><b>Description</b></td>',"\n";
         echo '<td class=color1><b>URL</b></td></tr>',"\n";
     }
     echo "</td></tr>\n";
-    echo '<input type=hidden name="rows" value='.$i.'>';
+    echo "<input type=hidden name='rows' value='$i'>";
 
     echo '<tr><td class=color1>New</td><td class=color1><input size=45% type="text" name="url_desc"></td>',"\n";
     echo '<td class=color1><input size=45% name="url" type="text"></td></tr>',"\n";
      
-    echo '<tr><td colspan=3 align=center class=color3><input type="submit" name=submit1 value="Update URL"></td></tr>',"\n";
+    echo '<tr><td colspan=3 align=center class=color3><input type="submit" name=submit value="Update URL"></td></tr>',"\n";
          
     echo '</table>',"\n";
     echo html_frame_end();
 
-    echo html_back_link(1,BASE."appview.php?appId=$appId");
+    echo html_back_link(1,BASE."appview.php?appId=$ob->appId");
 
 }
 
