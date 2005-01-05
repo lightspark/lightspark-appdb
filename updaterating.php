98938b4e64d7e1d7c63985addf295f928445fbe0
Paul van Schayck <polleke@gmail.com>
2005-01-05 05:26:03 +0000

    Don't redirect users without updating the rating first

--- updaterating.php
+++ updaterating.php
@@ -10,15 +10,8 @@ include("path.php");
 include(BASE."include/"."incl.php");
 include(BASE."include/"."rating.php");
 
-// go back to where we came from
-redirectref();
-
-apidb_header("Update Rating");
-
-apidb_session_start();
-
 rating_update($_GET);
 
-apidb_footer();
-
+// go back to where we came from
+redirectref();
 ?>
