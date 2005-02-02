6beff0b816350384c3d947a41837079cc025a8da
WineHQ <wineowner@winehq.org>
2005-02-02 01:11:59 +0000

    Add htmlarea http://www.htmlarea.com/ tool to appdb

---
+++ htmlarea/htmlarea_loader.js
@@ -0,0 +1,26 @@
+_editor_url = "../htmlarea/";
+_editor_lang = "en";
+document.writeln('<script type="text/javascript" src="../htmlarea/htmlarea.js"></script>');
+
+function initDocument() {
+    config = new HTMLArea.Config();
+    config.toolbar = [
+                [ "bold", "italic", "underline", "strikethrough", "separator",
+                  "copy", "cut", "paste", "space", "undo", "redo", "separator",
+                  "justifyleft", "justifycenter", "justifyright", "justifyfull", "separator",
+                  "orderedlist", "unorderedlist", "outdent", "indent", "separator",
+                  "forecolor", "hilitecolor", "separator",
+                  "inserthorizontalrule", "createlink", "inserttable" ]
+        ];
+    config.width = 700;
+    var editor = new HTMLArea("editor",config);
+    editor.config.pageStyle = "@import url(../application.css);";
+    editor.registerPlugin(DynamicCSS);
+    editor.generate();
+}
+
+onload = function() {
+    HTMLArea.loadPlugin("DynamicCSS");
+    HTMLArea.init();
+    HTMLArea.onload = initDocument;
+}
