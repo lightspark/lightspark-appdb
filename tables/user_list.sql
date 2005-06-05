ad3ae41fa279d35e2c262009589161baea60d825
Chris Morgan <cmorgan@alum.wpi.edu>
2005-06-05 18:30:34 +0000

    Remove some tabs and fix indenting.  Make email column a varchar(255) so we can mark it as unique, you can't have a unique text column in mysql.

--- tables/user_list.sql
+++ tables/user_list.sql
@@ -3,18 +3,18 @@ use apidb;
 drop table if exists user_list;
 
 create table user_list (
-	stamp           timestamp not null,
-        userid          int not null auto_increment,
-	password	text not null,
-	realname	text not null,
-	email		text not null,
-        created         datetime not null,
-        CVSrelease      text,
-        unique key(userid),
-	unique(email)
+  stamp           timestamp not null,
+  userid          int not null auto_increment,
+  password        text not null,
+  realname        text not null,
+  email           varchar(255) not null,
+  created         datetime not null,
+  CVSrelease      text,
+  unique key(userid),
+  unique(email)
 );
 
 insert into user_list values (NOW(), 0, password('testing'), 'Administrator',
-	'Admin@localhost', NOW(), 0, 0xffffffff);
+	'Admin@localhost', NOW(), 0);
 update user_list set userid = 1000 where email = 'Admin@localhost';
 
