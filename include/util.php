cd6f9e8302a7c307aa4be054a20fd5cff765179e
Chris Morgan <cmorgan@alum.wpi.edu>
2005-07-28 00:22:43 +0000

    Remove 'game' from our search terms because many application descriptions use the word and searching for it returns erroneous results

--- include/util.php
+++ include/util.php
@@ -344,7 +344,7 @@ function cleanupSearchWords($search_words)
     $ignore_words = array('I', 'a', 'about', 'an', 'are', 'as', 'at', 'be', 'by', 'com',
                           'de', 'en', 'for', 'from', 'how', 'in', 'is', 'it', 'la', 'of',
                           'on', 'or', 'that', 'the', 'this', 'to', 'was', 'what', 'when',
-                          'where', 'who', 'will', 'with', 'und', 'the', 'www');
+                          'where', 'who', 'will', 'with', 'und', 'the', 'www', 'game');
 
     $filtered_search = "";
 
