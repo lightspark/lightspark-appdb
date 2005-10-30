62077d5a108173b955186a15a5940e5e311417ee
Tony Lambregts <tony.lambregts@gmail.com>
2005-10-30 22:28:09 +0000

    Give administrators and maintainers a way to edit test results from distributionView.php

--- distributionView.php
+++ distributionView.php
@@ -132,6 +132,11 @@ else
             echo '<td>'.$oTest->sInstalls.'&nbsp</td>',"\n";
             echo '<td>'.$oTest->sRuns.'&nbsp</td>',"\n";
             echo '<td>'.$oTest->sTestedRating.'&nbsp</td>',"\n";
+            if ($_SESSION['current']->hasAppVersionModifyPermission($oTest->iVersionId))
+            {
+                echo '<td><a href="'.BASE.'/admin/adminTestResults.php?sub=view&iTestingId='.$oTest->iTestingId.'">',"\n";
+                echo 'Edit</a></td>',"\n";
+            }
             echo '</tr>',"\n";
         }
         echo '</table>',"\n";
