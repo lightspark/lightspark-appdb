2c37b1955b8c2dfdae2a399a38bc27cda556d81a
Jonathan Ernst <jonathan@ernstfamily.ch>
2006-07-07 17:02:07 +0000

    Enable filtering in search.php

--- search.php
+++ search.php
@@ -12,11 +12,8 @@
 // application environment
 include("path.php");
 require(BASE."include/incl.php");
+require(BASE."include/filter.php");
 require(BASE."include/application.php");
-
-$aClean = array(); //array of filtered user input
-
-$aClean['sSearchQuery'] = makeSafe($_REQUEST['sSearchQuery']);
 
 apidb_header("Search Results");
 perform_search_and_output_results($aClean['sSearchQuery']);
