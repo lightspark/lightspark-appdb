a9921788830fa1c7572a2ce83e355205ff9ec868
Chris Morgan <cmorgan@alum.wpi.edu>
2007-01-06 05:30:31 +0000

    Fix filtering of html, we shouldn't be running htmlspecialchars() on html code that needs to be preserved without modification

--- include/filter.php
+++ include/filter.php
@@ -52,7 +52,7 @@ function filter_gpc()
                 switch($aKeys[$i][1])
                 {
                      case "h": // HTML string
-                         $aClean[$aKeys[$i]] = trim(htmlspecialchars($_REQUEST[$aKeys[$i]]));
+                         $aClean[$aKeys[$i]] = trim($_REQUEST[$aKeys[$i]]);
                          // if there is no content and no image, make the variable empty
                          if(strip_tags($aClean[$aKeys[$i]],'<img>')=="")
                              $aClean[$aKeys[$i]] = "";
