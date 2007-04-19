2a58cf59af0442c185b23b46b41cdf63463fccc2
Alexander Nicolaysen Sørnes <alex@thehandofagony.com>
2007-04-19 23:19:24 +0000

    Conform testData_queue to OM standards

--- include/testData_queue.php
+++ include/testData_queue.php
@@ -18,7 +18,18 @@ class testData_queue
             $this->oDistribution->create();
             $this->oTestData->iDistributionId = $this->oDistribution->iDistributionId;
         }
-        $this->oTestData->create();
+        return $this->oTestData->create();
+    }
+
+    function delete()
+    {
+        return $this->oTestData->delete();
+    }
+
+    function unQueue()
+    {
+        $this->oTestData->unQueue();
+        $this->oDistribution->unQueue();
     }
 
     function update()
@@ -31,9 +42,17 @@ class testData_queue
     {
         $this->oTestData->outputEditor();
 
+        /* If we are processing queued test results with a queued distribution, we display
+           some additional help here */
+        if($this->oDistribution->sQueued != "false" && $this->canEdit())
+        {
+            echo "The user submitted a new distribution, which will be un-queued together ".
+                 "with the test data unless you select an existing one from the list above.";
+        }
+
         /* If the testData is already associated with a distribution and the distribution is
            un-queued, there is no need to display the distribution form here */
-        if(!$this->oTestData->iDistributionId or $this->oDistributionId->sQueued != "false")
+        if(!$this->oTestData->iDistributionId or $this->oDistribution->sQueued != "false")
             $this->oDistribution->outputEditor();
     }
 
@@ -56,6 +75,53 @@ class testData_queue
     function objectDisplayAddItemHelp()
     {
         $this->oTestData->objectDisplayAddItemHelp();
+    }
+
+    function objectGetEntries($bQueued, $bRejected)
+    {
+        return $this->oTestData->objectGetEntries($bQueued, $bRejected);
+    }
+
+    function objectGetHeader()
+    {
+        return $this->oTestData->objectGetHeader();
+    }
+
+    function objectGetInstanceFromRow($oRow)
+    {
+        return testData::objectGetInstanceFromRow($oRow);
+    }
+
+    function objectOutputTableRow($oObject, $sClass, $sEditLinkLabel)
+    {
+        return $this->oTestData->objectOutputTableRow($oObject, $sClass, $sEditLinkLabel);
+    }
+
+    function objectDisplayQueueProcessingHelp()
+    {
+        $oTest = new testData();
+        echo "<p>";
+        if($oTest->canEdit)
+            echo "This is the list of rejected test results, waiting to be resubmitted or deleted.";
+        else
+            echo "This is the list of your rejected test results.  Here you can make changes to ".
+                 "them and resubmit them into the database.";
+        echo "</p>\n";
+    }
+
+    function display()
+    {
+        return $this->oTest->display();
+    }
+
+    function objectMakeUrl()
+    {
+        return $this->oTest->objectMakeUrl();
+    }
+
+    function objectMakeLink()
+    {
+        return $this->oTest->objectMakeLink();
     }
 }
 
