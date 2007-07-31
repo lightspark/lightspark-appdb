8b7fdf606cfa73c5e2fa4e352e6242273b7df628
Chris Morgan <cmorgan@alum.wpi.edu>
2007-07-31 03:46:16 +0000

    No need to have 'View' in front of each of our links, it should be pretty obvious to admins that clicking on the link will let them interact with the particular queue/list.

--- include/sidebar_admin.php
+++ include/sidebar_admin.php
@@ -9,30 +9,30 @@ function global_admin_menu() {
 
     $g = new htmlmenu("Global Admin");
 
-    $g->add("View App Queue (".application::objectGetEntriesCount(true, false).")",
+    $g->add("App Queue (".application::objectGetEntriesCount(true, false).")",
             BASE."objectManager.php?sClass=application_queue&bIsQueue=true&sTitle=".
             "Application%20Queue");
-    $g->add("View Version Queue (".version::objectGetEntriesCount(true, false).")",
+    $g->add("Version Queue (".version::objectGetEntriesCount(true, false).")",
             BASE."objectManager.php?sClass=version_queue&bIsQueue=true&sTitle=".
             "Version%20Queue");
-    $g->add("View Screenshot Queue (".appData::objectGetEntriesCount("true",
+    $g->add("Screenshot Queue (".appData::objectGetEntriesCount("true",
             false, "screenshot").")",
             BASE."objectManager.php?sClass=screenshot&bIsQueue=true&sTitle=".
             "Screenshot%20Queue");
-    $g->add("View Maintainer Queue (".Maintainer::objectGetEntriesCount(true, false).")",
+    $g->add("Maintainer Queue (".Maintainer::objectGetEntriesCount(true, false).")",
             BASE."objectManager.php?sClass=maintainer&bIsQueue=true&sTitle=".
             "Maintainer%20Queue");
-    $g->add("View Test Results Queue (".testData::objectGetEntriesCount(true, false).")",
+    $g->add("Test Results Queue (".testData::objectGetEntriesCount(true, false).")",
             BASE."objectManager.php?sClass=testData_queue&bIsQueue=true&sTitle=".
             "Test%20Results%20Queue");
 
     $g->addmisc("&nbsp;");
 
-    $g->add("View Maintainer Entries (".Maintainer::getMaintainerCount().")",
+    $g->add("Maintainer Entries (".Maintainer::getMaintainerCount().")",
             BASE."admin/adminMaintainers.php");
-    $g->add("View Bug Links (".getNumberOfQueuedBugLinks()."/".getNumberOfBugLinks().")",
+    $g->add("Bug Links (".getNumberOfQueuedBugLinks()."/".getNumberOfBugLinks().")",
             BASE."admin/adminBugs.php");
-    $g->add("View Test Results (".testData::objectGetEntriesCount(false, false).")",
+    $g->add("Test Results (".testData::objectGetEntriesCount(false, false).")",
             BASE."objectManager.php?sClass=testData&bIsQueue=false&sTitle=".
             "View%20Test%20Results");
     $g->add("Users Management", BASE."admin/adminUsers.php");
@@ -41,14 +41,14 @@ function global_admin_menu() {
 
     $g->addmisc("&nbsp;");
 
-    $g->add("View Rejected Applications (".application::objectGetEntriesCount(true,
+    $g->add("Rejected Applications (".application::objectGetEntriesCount(true,
             true).")",
             BASE."objectManager.php?sClass=application_queue&bIsQueue=true&bIsRejected=".
             "true&sTitle=Rejected%20Applications");
-    $g->add("View Rejected Versions (".version::objectGetEntriesCount(true, true).")",
+    $g->add("Rejected Versions (".version::objectGetEntriesCount(true, true).")",
             BASE."objectManager.php?sClass=version_queue&bIsQueue=true&".
             "bIsRejected=true&sTitle=Rejected%20Versions");
-    $g->add("View Rejected Test Results (".testData::objectGetEntriesCount(true,
+    $g->add("Rejected Test Results (".testData::objectGetEntriesCount(true,
             true).")",
             BASE."objectManager.php?sClass=testData_queue&bIsQueue=true".
             "&bIsRejected=true&sTitle=Rejected%20Test%20Results");
