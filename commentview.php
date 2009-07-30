b8be6a22ce6e76489a745877fcc8c160143ac776
Alexander Nicolaysen Sørnes <alex@thehandofagony.com>
2009-07-30 21:53:49 +0200

    commentview.php: Don't allow unset or 0 versionId

--- commentview.php
+++ commentview.php
@@ -12,9 +12,15 @@ require("path.php");
 require(BASE."include/incl.php");
 require_once(BASE."include/comment.php");
 
+$iVersionId = getInput('iVersionId', $aClean);
+$iThreadId = getInput('iThreadId', $aClean);
+
+if(!$iVersionId)
+    util_show_error_page_and_exit('No versionId defined');
+
 apidb_header("Comments");
 
-Comment::view_app_comments($aClean['iVersionId'], $aClean['iThreadId']);
+Comment::view_app_comments($iVersionId, $iThreadId);
 
 apidb_footer();
 ?>
