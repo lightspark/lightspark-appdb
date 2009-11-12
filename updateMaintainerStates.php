c7745b5b18a331c9e02b1580a1af87da8002b77f
Alexander Nicolaysen Sørnes <alex@thehandofagony.com>
2009-11-12 10:54:33 +0100

    Fix admin check in updateMaintainerStates.php

--- updateMaintainerStates.php
+++ updateMaintainerStates.php
@@ -4,7 +4,7 @@ require_once('path.php');
 require_once('include/incl.php');
 require_once('include/maintainer.php');
 
-if($_SESSION['current']->hasPriv('admin'))
+if(!$_SESSION['current']->hasPriv('admin'))
     util_show_error_page_and_exit("Only admins are allowed in here");
 
 $hResult = application::objectGetEntries('accepted');
