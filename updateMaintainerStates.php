477a0396b550d8b0f2fe9b504376c0215af44ba2
Alexander Nicolaysen Sørnes <alex@thehandofagony.com>
2009-11-12 10:39:34 +0100

    Add a page for updating all apps' maintainer states

---
+++ updateMaintainerStates.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('path.php');
+require_once('include/incl.php');
+require_once('include/maintainer.php');
+
+if($_SESSION['current']->hasPriv('admin'))
+    util_show_error_page_and_exit("Only admins are allowed in here");
+
+$hResult = application::objectGetEntries('accepted');
+
+$i = 0;
+while($oRow = mysql_fetch_object($hResult))
+{
+    $oApp = new application(null, $oRow);
+    $oApp->updateMaintainerState();
+    $i++;
+}
+
+echo "Updated $i entries";
+
+?>
